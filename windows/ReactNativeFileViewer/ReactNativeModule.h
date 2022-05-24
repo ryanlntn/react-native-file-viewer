2ec9e714ba9a87941d3221fffa70040e7ad0063d
Ryan Linton <ryanlntn@gmail.com>
2022-05-24 11:38:25 -0600

    implement open using RNFS as a guide

--- windows/ReactNativeFileViewer/ReactNativeModule.h
+++ windows/ReactNativeFileViewer/ReactNativeModule.h
@@ -1,11 +1,17 @@
 ﻿#pragma once
 
+#include <filesystem>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Storage.h>
 #include "JSValue.h"
 #include "NativeModules.h"
 
 namespace RN = winrt::Microsoft::ReactNative;
 
+using namespace winrt;
+using namespace Windows::Foundation;
 using namespace winrt::Microsoft::ReactNative;
+using namespace winrt::Windows::Storage;
 
 namespace winrt::ReactNativeFileViewer
 {
@@ -22,13 +28,48 @@ struct ReactNativeModule
     }
     
     REACT_METHOD(Open, L"open")
-    void Open(std::string filepath, RN::ReactPromise<bool>&& promise) noexcept
+    winrt::fire_and_forget Open(std::string filepath, RN::ReactPromise<bool> promise) noexcept
+    try 
     {
-        promise.Resolve(true);
+        winrt::hstring directoryPath, fileName;
+        splitPath(filepath, directoryPath, fileName);
+
+        StorageFolder folder{ co_await StorageFolder::GetFolderFromPathAsync(directoryPath) };
+        StorageFile file{ co_await folder.GetFileAsync(fileName) };
+        if (file != nullptr) 
+        {
+            bool success{ co_await Windows::System::Launcher::LaunchFileAsync(file) };
+            if (success) 
+            {
+                promise.Resolve(true);
+            }
+            else 
+            {
+                promise.Reject(RN::ReactError{ "Error opening the file" + filepath });
+            }
+        }
+        else
+        {
+            promise.Reject(RN::ReactError{ "Error opening the file" + filepath });
+        }
     }
+    catch (const hresult_error& ex) {
+        promise.Reject(winrt::to_string(ex.message()).c_str());
+    }
+
+    
 
     private:
         ReactContext m_reactContext{nullptr};
+
+        void splitPath(const std::string& fullPath, winrt::hstring& directoryPath, winrt::hstring& fileName) noexcept
+        {
+            std::filesystem::path path(fullPath);
+            path.make_preferred();
+
+            directoryPath = path.has_parent_path() ? winrt::to_hstring(path.parent_path().c_str()) : L"";
+            fileName = path.has_filename() ? winrt::to_hstring(path.filename().c_str()) : L"";
+        }
 };
 
 } // namespace winrt::ReactNativeFileViewer
