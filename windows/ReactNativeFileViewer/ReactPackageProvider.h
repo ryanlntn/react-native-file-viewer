66a482b1531a832891190a316272ca42543895c8
Ryan Linton <ryanlntn@gmail.com>
2022-05-26 09:29:59 -0600

    register as RNFileViewer

--- windows/ReactNativeFileViewer/ReactPackageProvider.h
+++ windows/ReactNativeFileViewer/ReactPackageProvider.h
@@ -3,7 +3,7 @@
 
 using namespace winrt::Microsoft::ReactNative;
 
-namespace winrt::ReactNativeFileViewer::implementation
+namespace winrt::RNFileViewer::implementation
 {
     struct ReactPackageProvider : ReactPackageProviderT<ReactPackageProvider>
     {
@@ -11,11 +11,11 @@ namespace winrt::ReactNativeFileViewer::implementation
 
         void CreatePackage(IReactPackageBuilder const &packageBuilder) noexcept;
     };
-} // namespace winrt::ReactNativeFileViewer::implementation
+} // namespace winrt::RNFileViewer::implementation
 
-namespace winrt::ReactNativeFileViewer::factory_implementation
+namespace winrt::RNFileViewer::factory_implementation
 {
 
 struct ReactPackageProvider : ReactPackageProviderT<ReactPackageProvider, implementation::ReactPackageProvider> {};
 
-} // namespace winrt::ReactNativeFileViewer::factory_implementation
+} // namespace winrt::RNFileViewer::factory_implementation
